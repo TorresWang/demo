882956de608d25f17b851d20562c8c6db71401c5
TorresWang <t9_wangjy@qq.com>
2019-09-26 17:26:23 +0800

    new file:   .gitignore 	new file:   .mvn/wrapper/MavenWrapperDownloader.java 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/demo/DemoApplication.java 	new file:   src/main/java/com/demo/controller/HelloController.java 	new file:   src/main/java/com/demo/pojo/User.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/demo/demo/DemoApplicationTests.java

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.pojo.User;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/getUser")
+	public User getUser() {
+		User user = new User();
+		user.setName("wjy");
+		user.setPassword("ftorres9");
+		return user;
+	}
+}
