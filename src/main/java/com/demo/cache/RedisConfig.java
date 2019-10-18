1a2a32e789a8aaee2a3137dec614974929ffcdca
TorresWang <t9_wangjy@qq.com>
2019-10-18 16:18:09 +0800

    modified:   src/main/java/com/demo/cache/RedisConfig.java 	modified:   src/main/java/com/demo/controller/HelloController.java

--- src/main/java/com/demo/cache/RedisConfig.java
+++ src/main/java/com/demo/cache/RedisConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableCaching
+@EnableCaching//开启缓存
 public class RedisConfig extends CachingConfigurerSupport {
 	
 	@Bean
